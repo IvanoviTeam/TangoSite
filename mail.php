17f1e9a501697e41dd812c9193527376037e2633
Poodles <zombaza@abv.bg>
2014-11-27 23:58:36 +0200

    Responsive products fixed
    title

--- mail.php
+++ mail.php
@@ -4,9 +4,9 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone \n Message: $message";
-$recipient = "zombaza@abv.bg";
+$recipient = "gravitycats@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+@mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
